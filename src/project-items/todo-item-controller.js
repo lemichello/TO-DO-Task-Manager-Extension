88d7d6bba2aa27d1b36f64e629f698d08d0a1cf9
Lemich Maksym <lemichello.m@gmail.com>
2019-10-04 20:19:14 +0300

    feat(todoItem): added information about project and deadline

--- src/project-items/todo-item-controller.js
+++ src/project-items/todo-item-controller.js
@@ -9,6 +9,18 @@
 
         ctrl.$onInit = function () {
             ctrl.isWithoutNotes = ctrl.item.notes === "";
+
+            if (ctrl.item.deadline !== '1754-01-01T00:00:00') {
+                let todayDate = new Date().getTime();
+                let itemDeadline = Date.parse(ctrl.item.deadline);
+
+                if (todayDate > itemDeadline) {
+                    ctrl.deadlineStr = 'today';
+                } else {
+                    let daysLeft = Math.ceil((itemDeadline - todayDate) / (1000 * 3600 * 24));
+                    ctrl.deadlineStr = `${daysLeft}d left`;
+                }
+            }
         };
     }
 })();
