8c6d5563c2c255fc166e3fe6527df88b711facee
Lemich Maksym <lemichello.m@gmail.com>
2019-10-06 16:33:14 +0300

    feat(*): added possibility edit tasks

--- src/todo-item-form/todo-item-form-controller.js
+++ src/todo-item-form/todo-item-form-controller.js
@@ -11,7 +11,9 @@
 
         ctrl.minDate = new Date();
 
-        ctrl.item = todoItem;
+        ctrl.item = {};
+        Object.assign(ctrl.item, todoItem);
+
         ctrl.isNew = isNew;
 
         ctrl.sharedProjects = sharedProjects;
@@ -20,10 +22,14 @@
         ctrl.$onInit = function () {
             if (ctrl.item.date === '1754-01-01T00:00:00') {
                 ctrl.item.date = null;
+            } else if (typeof ctrl.item.date === 'string') {
+                ctrl.item.date = new Date(ctrl.item.date);
             }
 
             if (ctrl.item.deadline === '1754-01-01T00:00:00') {
                 ctrl.item.deadline = null;
+            } else if (typeof ctrl.item.deadline === 'string') {
+                ctrl.item.deadline = new Date(ctrl.item.deadline);
             }
 
             ctrl.selectedSharedProject = ctrl.item.projectId;
@@ -39,14 +45,21 @@
 
             ctrl.item.projectId = ctrl.selectedSharedProject;
 
-            TodoItemService.addItem(ctrl.item).then(function () {
-                $state.go('main');
-            });
+            if (ctrl.isNew) {
+                TodoItemService.addItem(ctrl.item).then(function () {
+                    $state.go('main');
+                });
+            } else {
+                TodoItemService.updateItem(ctrl.item).then(function () {
+                    $state.go('main');
+                });
+            }
         };
 
         ctrl.deleteItem = function () {
-            TodoItemService.deleteItem(ctrl.item);
-            $state.go('main');
+            TodoItemService.deleteItem(ctrl.item).then(function () {
+                $state.go('main');
+            });
         };
     }
 })();
--- src/todo-item/todo-item-service.js
+++ src/todo-item/todo-item-service.js
@@ -1,123 +1,124 @@
 (function () {
-    "use strict";
+        "use strict";
 
-    angular.module('extension')
-        .service('TodoItemService', TodoItemService);
+        angular.module('extension')
+            .service('TodoItemService', TodoItemService);
 
-    TodoItemService.$inject = ['$mdDialog', '$http', 'ApiBasePath'];
+        TodoItemService.$inject = ['$mdDialog', '$http', 'ApiBasePath'];
 
-    function TodoItemService($mdDialog, $http, ApiBasePath) {
-        let service = this;
+        function TodoItemService($mdDialog, $http, ApiBasePath) {
+            let service = this;
 
-        service.getFormattedDate = function (date) {
-            let day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
-            return `${date.getFullYear()}-${date.getMonth() + 1}-${day}T00:00:00`;
-        };
+            service.getFormattedDate = function (date) {
+                let day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
+                return `${date.getFullYear()}-${date.getMonth() + 1}-${day}T00:00:00`;
+            };
 
-        service.getDeadlineStr = function (item) {
-            let todayDate = new Date().getTime();
-            let itemDeadline = Date.parse(item.deadline);
+            service.getDeadlineStr = function (item) {
+                let todayDate = new Date().getTime();
+                let itemDeadline = Date.parse(item.deadline);
 
-            if (todayDate > itemDeadline) {
-                return 'today';
-            } else {
-                let daysLeft = Math.ceil((itemDeadline - todayDate) / (1000 * 3600 * 24));
-                return `${daysLeft}d left`;
-            }
-        };
+                if (todayDate > itemDeadline) {
+                    return 'today';
+                } else {
+                    let daysLeft = Math.ceil((itemDeadline - todayDate) / (1000 * 3600 * 24));
+                    return `${daysLeft}d left`;
+                }
+            };
 
-        service.uncheckLoggedItem = function (item, removeMethod) {
-            let confirm = $mdDialog.confirm()
-                .title('Uncheck TO-DO item')
-                .textContent('This TO-DO is already logged. Are you sure you want to mark it as incomplete?')
-                .ariaLabel('Question')
-                .ok('Yes')
-                .cancel('No');
+            service.uncheckLoggedItem = function (item, removeMethod) {
+                let confirm = $mdDialog.confirm()
+                    .title('Uncheck TO-DO item')
+                    .textContent('This TO-DO is already logged. Are you sure you want to mark it as incomplete?')
+                    .ariaLabel('Question')
+                    .ok('Yes')
+                    .cancel('No');
 
-            $mdDialog.show(confirm).then(function () {
-                item.completeDate = '1754-01-01T00:00:00';
-                updateItem(item).then(function (result) {
-                    removeMethod({item: item});
+                $mdDialog.show(confirm).then(function () {
+                    item.completeDate = '1754-01-01T00:00:00';
+                    service.updateItem(item).then(function (result) {
+                        removeMethod({item: item});
+                    }).catch(function () {
+                        return false;
+                    })
                 }).catch(function () {
                     return false;
+                });
+            };
+
+            service.checkItem = function (item, removeMethod) {
+                let todayDate = new Date();
+                let day = todayDate.getUTCDate() > 9 ? todayDate.getUTCDate() : `0${todayDate.getUTCDate()}`;
+
+                item.completeDate = `${todayDate.getUTCFullYear()}-${todayDate.getUTCMonth() + 1}-${day}T${todayDate.getUTCHours()}:${todayDate.getUTCMinutes()}:${todayDate.getUTCSeconds()}.${todayDate.getUTCMilliseconds()}`;
+
+                service.updateItem(item).then(function (result) {
+                    if (result) {
+                        removeMethod({item: item});
+                    }
+                }).catch(function () {
+                    return false;
+                });
+            };
+
+            service.addItem = function (item) {
+                return $http({
+                    method: 'POST',
+                    url: `${ApiBasePath}/todoitems`,
+                    data: JSON.stringify(item),
+                    mode: 'cors',
+                    withCredentials: true
+                }).then(function (response) {
+                    if (response.status !== 200) {
+                        alert('Not added');
+                        return false;
+                    }
+
+                    return true;
+                }).catch(function () {
+                    alert('Server error');
+                    return false;
                 })
-            }).catch(function () {
-                return false;
-            });
-        };
+            };
 
-        service.checkItem = function (item, removeMethod) {
-            let todayDate = new Date();
-            let day = todayDate.getUTCDate() > 9 ? todayDate.getUTCDate() : `0${todayDate.getUTCDate()}`;
+            service.deleteItem = function (item) {
+                return $http({
+                    method: 'DELETE',
+                    url: `${ApiBasePath}/todoitems/${item.id}`,
+                    mode: 'cors',
+                    withCredentials: true
+                }).then(function (response) {
+                    if (response.data.statusCode !== 200) {
+                        alert('Not deleted');
+                        return false;
+                    }
 
-            item.completeDate = `${todayDate.getUTCFullYear()}-${todayDate.getUTCMonth() + 1}-${day}T${todayDate.getUTCHours()}:${todayDate.getUTCMinutes()}:${todayDate.getUTCSeconds()}.${todayDate.getUTCMilliseconds()}`;
-
-            updateItem(item).then(function (result) {
-                if (result) {
-                    removeMethod({item: item});
-                }
-            }).catch(function () {
-                return false;
-            });
-        };
-
-        service.addItem = function (item) {
-            return $http({
-                method: 'POST',
-                url: `${ApiBasePath}/todoitems`,
-                data: JSON.stringify(item),
-                mode: 'cors',
-                withCredentials: true
-            }).then(function (response) {
-                if (response.status !== 200) {
-                    alert('Not added');
+                    return true;
+                }).catch(function () {
+                    alert('Server error');
                     return false;
-                }
+                })
+            };
 
-                return true;
-            }).catch(function () {
-                alert('Server error');
-                return false;
-            })
-        };
+            service.updateItem = function (item) {
+                return $http({
+                    method: 'PUT',
+                    url: `${ApiBasePath}/todoitems`,
+                    data: JSON.stringify(item),
+                    mode: 'cors',
+                    withCredentials: true
+                }).then(function (response) {
+                    if (response.data.statusCode !== 200) {
+                        alert('Not modified');
+                        return false;
+                    }
 
-        service.deleteItem = function (item) {
-            return $http({
-                method: 'DELETE',
-                url: `${ApiBasePath}/todoitems/${item.id}`,
-                mode: 'cors',
-                withCredentials: true
-            }).then(function (response) {
-                if (response.data.statusCode !== 200) {
-                    alert('Not deleted');
+                    return true;
+                }).catch(function () {
+                    alert('Server error');
                     return false;
-                }
-
-                return true;
-            }).catch(function () {
-                alert('Server error');
-                return false;
-            })
-        };
-
-        function updateItem(item) {
-            return $http({
-                method: 'PUT',
-                url: `${ApiBasePath}/todoitems`,
-                data: JSON.stringify(item),
-                mode: 'cors',
-                withCredentials: true
-            }).then(function (response) {
-                if (response.data.statusCode !== 200) {
-                    alert('Not modified');
-                    return false;
-                }
-
-                return true;
-            }).catch(function () {
-                alert('Server error');
-                return false;
-            });
+                });
+            }
         }
     }
-})();
+)();
